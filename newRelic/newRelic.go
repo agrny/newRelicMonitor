712f8fbdff326f39edeb3ce4c36af458c5409ac9
Alec Greenaway <Alec.Greenaway@geaerospace.com>
2026-03-20 13:04:05 -0700

    replaced new relic import with http client based struct

--- newRelic/newRelic.go
+++ newRelic/newRelic.go
@@ -2,24 +2,32 @@
 package newrelic
 
 import (
-	"os"
-	"time"
+	"fmt"
+	"net/http"
 
 	"new-relic-monitor/config"
-
-	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
-func NewApplicationWithConfig(options config.Config) (*newrelic.Application, error) {
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigAppName("S3 Backup Monitor"),
-		newrelic.ConfigLicense(options.NewRelicLicenseKey),
-		newrelic.ConfigDebugLogger(os.Stdout),
-		newrelic.ConfigAppLogForwardingEnabled(true),
-	)
-	if err != nil {
-		return nil, err
+type NewRelicClient struct {
+	NewRelicLicenseKey string
+	AppName            string
+	APIURL             string
+	Client             *http.Client
+}
+
+func NewClientWithConfig(options config.Config) (*NewRelicClient, error) {
+	if options.NewRelicLicenseKey == "" {
+		return nil, fmt.Errorf("new relic license key required")
 	}
-	app.WaitForConnection(5 * time.Second)
-	return app, nil
+
+	return &NewRelicClient{
+		NewRelicLicenseKey: options.NewRelicLicenseKey,
+		Client:             &http.Client{},
+	}, nil
+}
+
+func (nr *NewRelicClient) RecordCustomEvent(eventype string, data map[string]any) {
+
+
+
 }
